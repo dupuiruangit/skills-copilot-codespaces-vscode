74014ccb01142626b444a62c84ae7a99ff2228d9
dupuiruangit <ruandupuigit@gmail.com>
2025-03-25 13:26:29 +0000

    Third commit do Copilot

--- comments.js
+++ comments.js
@@ -1,14 +1,14 @@
-// comments.js
-const express = require('express');
-const app = express();
-
-// Rota principal
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// Inicia o servidor na porta 3000
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. Use the comments.html file from the previous exercise.
+// The comments.html file should be served as a static file. Use the fs module to read the file and send it to the client.
+// The server should listen on port 3000.
+// The server should respond to GET requests to the /comments URL.
+// The server should respond with the contents of the comments.html file.
+// The server should respond with a 200 status code.
+// The server should respond with the Content-Type set to text/html.
+// The server should respond with the contents of the comments.html file.
+// The server should respond with a 200 status code.
+// The server should respond with the Content-Type set to text/html.
+// The server should respond with the contents of the comments.html file.
+// The server should respond with a 200 status code.
+// The server should respond with the Content-Type set to text/html.
